32331920e183586a26f965f71dd1866ff8d6b022
skidev101 <skidev101@gmail.com>
2026-02-17 04:55:14 +0100

    chore: ui

--- js/chat.js
+++ js/chat.js
@@ -50,7 +50,7 @@ async function loadProfile() {
 
     userName.textContent = user.email?.split("@")[0];
     profileInitial.textContent =
-      user.name?.charAt(0).toUpperCase() ?? "?";
+      user.name?.charAt(0).toUpperCase() ?? "SP";
   } catch (err) {
     console.error(err.message);
   }
